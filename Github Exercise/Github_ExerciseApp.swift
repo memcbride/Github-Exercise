ee6b5a947d7e43c050b4c81c8cf0435b85601302
MacModeler <memcbride@mac.com>
2024-06-16 19:11:25 -0400

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Mark McBride on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
